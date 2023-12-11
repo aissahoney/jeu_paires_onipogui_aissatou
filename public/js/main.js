0e7c1c9ae0830c75a96cb0f73f82780bb69eaabb
aissahoney <aissatou.onipogui@gmail.com>
2023-12-11 16:51:55 +0100

    test click case

--- public/js/main.js
+++ public/js/main.js
@@ -7,17 +7,54 @@ let src6="./public/img/six.webp"
 let src7="./public/img/seven.webp"
 
 let cards=[src2,src3,src4];
-console.log(cards)
+// let srcRandom=cards[Math.floor(Math.random()*cards.length)]
+// console.log(srcRandom)
 
-let place=[];
-    for (let i = cards.length-1; cards.length>0; i--) {
-        place.push(cards[i])
-        cards.splice(i,1)
-    }
-console.log(cards) 
-console.log(place)
+// let place=[];
+//     for (let i = cards.length-1; cards.length>0; i--) {
+        
+//         cards.splice(i,1)
+//     }
+// console.log(cards) 
+// console.log(place)
 
 
 
-let lineOne= document.querySelector(".first-line")
-let lineTwo= document.querySelector(".second-line")
+let col=document.querySelectorAll(".col");
+// console.log(col)
+let img= document.querySelectorAll("img")
+// console.log(img)
+
+col.forEach(element => {
+    element.addEventListener("click", function (e) {
+        col= e.target
+        console.log(element)
+        Array.from(element).forEach(items => {
+            console.log(items)
+            // if (element.src=="back-img") {
+            //     element.style.display="none"
+            
+            // } 
+            // if(element.src == "") {
+            //     element.src = cards[Math.floor(Math.random()*cards.length)]
+            //     element.style.display="block"
+            // }
+            
+        });
+       
+        // let img = element.childNodes
+        // console.log(img)
+        // for (let i = 0; i < img.length; i++) {
+        //     console.log(img[i].src)
+        //     // if (i==0) {
+        //     //     console.log(img[i])
+        //     // //    img[i].style.display= "none"
+        //     // }
+        //     // if (i==1) {
+        //     //     console.log(img[i])
+        //     //     // img[i].src=cards[Math.floor(Math.random()*cards.length)]
+        //     //     // img[i].style.display = "block"  
+        //     // }
+        // }
+    })
+})
