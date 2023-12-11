2148a9fe530f8f8898c6884fced44bd9fa4fb909
aissahoney <aissatou.onipogui@gmail.com>
2023-12-11 22:40:28 +0100

    methode distribution des cartes

--- public/js/main.js
+++ public/js/main.js
@@ -5,8 +5,8 @@ let src4="./public/img/four.webp"
 let src5="./public/img/five.webp"
 let src6="./public/img/six.webp"
 let src7="./public/img/seven.webp"
-
 let sevenCards=[src1,src2,src3,src4,src5,src6,src7]
+// ------choix des cartes------------------
 let cardsOfThree=[]
 let i=0
 while (i<3) {
@@ -16,13 +16,19 @@ while (i<3) {
     cardsOfThree.push(y)
 i++
 }
-let cards=cardsOfThree.concat(cardsOfThree)
-console.log(cards)
+let cards=cardsOfThree.concat(cardsOfThree) //pour avoir chaque element en double dans tableau
+// console.log(cards)         
 
 let col=document.querySelectorAll(".col");
-// console.log(col)
+// console.log(col)           
 let img= document.querySelectorAll("img")
 // console.log(img)
+img.forEach(element => {
+    if (element.classList.contains("frontImg")) {
+        element.src =cards.splice(Math.floor(Math.random()*cards.length),1) ;
+    console.log(cards)
+    }
+});
 
 col.forEach(element => {
     element.addEventListener("click", function () {
@@ -35,9 +41,12 @@ col.forEach(element => {
                 
             } 
             if(item.classList.contains("frontImg")) {
-                item.src = cards[Math.floor(Math.random()*cards.length)]
+                // img.src = cards[Math.floor(Math.random()*cards.length)]
+                // cards.slice(img.src)
+                // var x = ['a','b','c','d']; 
+                // img.src =`"${cards.splice(Math.floor(Math.random()*cards.length),1)}"` ;
                 item.style.display="block"
-                // console.log(item)
+                // console.log(cards)
             }
             
         }); 
